7d85d94b3defb13694422bcfb9646dc03f944b76
Felipe DR <felipe.corp7@gmail.com>
2024-05-09 16:50:15 -0300

    feat: class-02 structuring the base of the mono-repo

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,7 +1,12 @@
+import { Text } from "@alura/design-system/components/text";
+
+import { sum } from "@alura/utils/math/sum";
+
 export default function HomeScreen() {
   return (
-    <div>
-      <h1>Home do Projeto</h1>
-    </div>
+    <main>
+      <Text tag="h1">Home</Text>
+      <Text tag="p">@alura/utils/math/sum: sum(2,2) = {sum(2, 2)}</Text>
+    </main>
   );
 }
